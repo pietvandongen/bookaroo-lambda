6138ee85860b879fe50143bc6d7b0d2cc9a3255d
Piet van Dongen <piet.vandongen@luminis.eu>
2019-03-01 13:46:24 +0100

    Scaffolding

--- src/main/java/eu/luminis/bookaroo/HelloWorldStreamHandler.java
+++ src/main/java/eu/luminis/bookaroo/HelloWorldStreamHandler.java
@@ -1,27 +1,14 @@
-/*
-     Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-     Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
-     except in compliance with the License. A copy of the License is located at
-
-         http://aws.amazon.com/apache2.0/
-
-     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
-     the specific language governing permissions and limitations under the License.
-*/
-
-package com.amazon.ask.helloworld;
+package eu.luminis.bookaroo;
 
 import com.amazon.ask.Skill;
 import com.amazon.ask.SkillStreamHandler;
 import com.amazon.ask.Skills;
-import com.amazon.ask.helloworld.handlers.CancelandStopIntentHandler;
-import com.amazon.ask.helloworld.handlers.FallbackIntentHandler;
-import com.amazon.ask.helloworld.handlers.HelloWorldIntentHandler;
-import com.amazon.ask.helloworld.handlers.HelpIntentHandler;
-import com.amazon.ask.helloworld.handlers.LaunchRequestHandler;
-import com.amazon.ask.helloworld.handlers.SessionEndedRequestHandler;
+import eu.luminis.bookaroo.handlers.CancelandStopIntentHandler;
+import eu.luminis.bookaroo.handlers.FallbackIntentHandler;
+import eu.luminis.bookaroo.handlers.HelloWorldIntentHandler;
+import eu.luminis.bookaroo.handlers.HelpIntentHandler;
+import eu.luminis.bookaroo.handlers.LaunchRequestHandler;
+import eu.luminis.bookaroo.handlers.SessionEndedRequestHandler;
 
 public class HelloWorldStreamHandler extends SkillStreamHandler {
 
--- src/main/java/eu/luminis/bookaroo/handlers/FallbackIntentHandler.java
+++ src/main/java/eu/luminis/bookaroo/handlers/FallbackIntentHandler.java
@@ -1,4 +1,4 @@
-package com.amazon.ask.helloworld.handlers;
+package eu.luminis.bookaroo.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
@@ -8,9 +8,6 @@ import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
 
-// 2018-July-09: AMAZON.FallackIntent is only currently available in en-US locale.
-//              This handler will not be triggered except in that locale, so it can be
-//              safely deployed for any locale.
 public class FallbackIntentHandler implements RequestHandler {
 
     @Override
